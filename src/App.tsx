8ec7ffe43bf8f92f01d2487c3c239fe3880ea8bc
yeonlisa <tkzw4869@gmail.com>
2022-03-11 17:43:28 +0900

    Functional Component type

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,10 @@
 import React from 'react';
 import './App.css';
 
-let name: any;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-let printName: (name: string) => never;
-
-// type Person = {
-//   name: string;
-//   age?: number;
-// };
-
-// let person: Person = {
-//   name: 'Lisa',
-// };
-
-// let lotsOfPeople: Person[];
-
-// let personName: unknown;
-
-interface Person {
-  name: string;
-  age?: number;
-};
-
-type X = {
-  a: string;
-  b: number;
-};
-
-type Y = {
-  c: string;
-  d: number;
-};
-
-let y: Y = {
-  c: 'efdas',
-  d: 42,
-}
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      Hello World
+      <span className="heading">Taskify</span>
     </div>
   );
 }
