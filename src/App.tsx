4c215a9bf6e6c47932f108b36aed91680b616bde
yeonlisa <tkzw4869@gmail.com>
2022-03-11 17:27:08 +0900

    Basic Types in Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+role = [5, "5"];
+
 function App() {
   return (
     <div className="App">
-
+      Hello World
     </div>
   );
 }
